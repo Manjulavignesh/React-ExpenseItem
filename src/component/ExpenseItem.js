405dc97bd2648cca4fd1328f4fbad8d10f0120b0
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-06 13:09:31 +0530

    branch-3

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,10 +1,14 @@
 function ExpenseItem()
 {
+    const expenseDate=new Date(2021,2,27);
+    const expenseItem="car Insurance";
+    const expenseAmt=700;
+    const LocationOfExpenditure="Insurance"
     return <div>
-        <h2>ExpenseItem:</h2>
-        <h4>Food Rs 10</h4>
-        <h4>petrol Rs 100</h4>
-        <h4>Movies Rs 200</h4>
+        <div><h2>{expenseDate.toISOString()}</h2></div>
+        <div><h2>{expenseItem}</h2></div>
+        <div><h2>{expenseAmt}</h2></div>
+        <div><h2>{LocationOfExpenditure}</h2></div>
         </div>
 }
 export default ExpenseItem;
