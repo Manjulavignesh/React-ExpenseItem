506d7358bd9b2d0c0034b368779614305b119d15
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-06 13:47:49 +0530

    App.props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import ExpenseItem from './component/ExpenseItem';
-import './App.css';
 function App() {
+  const expense=[{title:"food",amt:"Rs 10",LocationofExpenditure:"Hotel"},
+                 {title:"petrol",amt:"Rs 200",LocationofExpenditure:"petrolbunk"}]
   return (
     <div >
-        <ExpenseItem></ExpenseItem>
+      <h2>ExpenseItem:</h2>
+        <ExpenseItem title={expense[0].title} amt={expense[0].amt} LocationofExpenditure={expense[0].LocationofExpenditure}></ExpenseItem>
+        <ExpenseItem title={expense[1].title} amt={expense[1].amt} LocationofExpenditure={expense[1].LocationofExpenditure}></ExpenseItem>
     </div>
   );
 }
