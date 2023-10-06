2e2be13938fd593d06de3ad949e07886d4e8bcf2
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-06 15:11:23 +0530

    Expensedate

---
+++ src/component/ExpenseDate.js
@@ -0,0 +1,12 @@
+function ExpenseDate(props)
+{
+    const Month=props.date.toLocaleString('en-US',{month:'long'});
+    const day=props.date.toLocaleString('en-US',{day:'2-digit'});
+    const year=props.date.getFullYear();
+return (<div>
+<div>{Month}</div>
+<div>{year}</div>
+<div>{day}</div>
+</div>);
+}
+export default ExpenseDate;
