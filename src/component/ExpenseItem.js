bec7c314f20e03cc361365cf116c6b61a8f907ff
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-06 13:48:29 +0530

    propsconcept

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,14 +1,9 @@
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const expenseDate=new Date(2021,2,27);
-    const expenseItem="car Insurance";
-    const expenseAmt=700;
-    const LocationOfExpenditure="Insurance"
     return <div>
-        <div><h2>{expenseDate.toISOString()}</h2></div>
-        <div><h2>{expenseItem}</h2></div>
-        <div><h2>{expenseAmt}</h2></div>
-        <div><h2>{LocationOfExpenditure}</h2></div>
+        <div><h2>{props.title}</h2></div>
+        <div><h2>{props.amt}</h2></div>
+        <div><h2>{props.LocationofExpenditure}</h2></div>
         </div>
 }
 export default ExpenseItem;
