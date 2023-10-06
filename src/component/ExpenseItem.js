940de98e8d226640fd1d48a080681ebf8049273d
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-06 12:29:34 +0530

    branch-1

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem()
+{
+    return <div>
+        <h2>ExpenseItem:</h2>
+        <h4>Food Rs 10</h4>
+        <h4>petrol Rs 100</h4>
+        <h4>Movies Rs 200</h4>
+        </div>
+}
+export default ExpenseItem;
