d08200a8efcbb080e11f7fbc74fd4f9466767da2
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-07 17:55:00 +0530

    adding input value

--- src/component/ExpenseForm.js
+++ src/component/ExpenseForm.js
@@ -1,18 +1,30 @@
+import React,{useState} from 'react';
 const ExpenseForm=()=>
 {
-    function evenhandler(e) 
+    const [enteredTitle,setEnteredTitle]=useState('');
+    const [enteredAmt,setEnteredAmt]=useState('');
+    const [enteredDate,setEnteredDate]=useState('');
+    function Titlehandler(e) 
     {
-        console.log(e.target.value);
+        setEnteredTitle(e.target.value);
+    }
+    function amthandler(e) 
+    {
+        setEnteredAmt(e.target.value);
+    }
+    function datehandler(e) 
+    {
+        setEnteredDate(e.target.value);
     }
 return (
     <div>
         <form>
             <label>ExpenseTitle:</label>
-        <input type="text" id="text" autoComplete="off" onChange={evenhandler}/>
+        <input type="text" id="text" autoComplete="off" onChange={Titlehandler}/>
             <label>ExpenseAmt:</label>
-        <input type="number" id="number" autoComplete="off" onChange={evenhandler}/>
+        <input type="number" id="number" autoComplete="off" onChange={amthandler}/>
             <label>ExpenseDate:</label>
-        <input type="date" id="date" autoComplete="off" onChange={evenhandler}/>
+        <input type="date" id="date" autoComplete="off" onChange={datehandler}/>
         <button>Submit</button>
         </form>
     </div>
