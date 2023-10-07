570b8d69161d62c16711f48ba870046c4e6011e3
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-07 10:38:57 +0530

    using arrow function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import ExpenseItem from './component/ExpenseItem';
-function App() {
-  const expense=[{title:"food",amt:"Rs 10",LocationofExpenditure:"Hotel"},
-                 {title:"petrol",amt:"Rs 200",LocationofExpenditure:"petrolbunk"}]
+const App = ()=> {
+  const expense=[{title:"food",amt:"Rs 10",LocationofExpenditure:"Hotel" ,date:new Date(2021,2,27)},
+                 {title:"petrol",amt:"Rs 200",LocationofExpenditure:"petrolbunk",date:new Date(2020,7,15)}]
   return (
     <div >
       <h2>ExpenseItem:</h2>
-        <ExpenseItem title={expense[0].title} amt={expense[0].amt} LocationofExpenditure={expense[0].LocationofExpenditure}></ExpenseItem>
-        <ExpenseItem title={expense[1].title} amt={expense[1].amt} LocationofExpenditure={expense[1].LocationofExpenditure}></ExpenseItem>
+        <ExpenseItem title={expense[0].title} amt={expense[0].amt} Location={expense[0].LocationofExpenditure} date={expense[0].date}/>
+        <ExpenseItem title={expense[1].title} amt={expense[1].amt} Location={expense[1].LocationofExpenditure}date={expense[1].date}/>
     </div>
   );
 }
