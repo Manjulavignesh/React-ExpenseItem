038990862f0f867bac3b9e7afa42ba993584edfb
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-07 11:13:21 +0530

    adding button

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -2,9 +2,13 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 const ExpenseItem = (props) =>
 {
+    const eventhandler=()=>{
+    console.log("deleted");
+    }
     return <div>
         <ExpenseDate date={props.date}/>
         <ExpenseDetails title={props.title} amt={props.amt} location={props.location}/>
+        <button onClick={eventhandler}>Delete Expense</button>
         </div>
 }
 export default ExpenseItem;
