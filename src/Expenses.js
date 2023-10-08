58a78690ee06b2fc8e93b2201db62ea452146ece
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-08 19:57:50 +0530

    adding filter function

--- src/Expenses.js
+++ src/Expenses.js
@@ -4,20 +4,26 @@ import ExpenseFilter from "./component/ExpenseFilter";
 const Expenses = (props) => {
   const [filteredyear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedyear) => {
-    setFilteredYear(selectedyear)
+    setFilteredYear(selectedyear);
   };
-  return (<div>
-  <ExpenseFilter selected={filteredyear} onfilter={filterChangeHandler} />;
-  {
-    props.items.map((expense,index) => (
-      <ExpenseItem
-      key={index}
-        title={expense.title}
-        amt={expense.amt}
-        date={expense.date}
-      />
-  ))
-}
-  </div>);
-}
+  const newItems=props.items.filter((item)=>{
+    if(item.date.getFullYear()==filteredyear)
+    {
+      return item;
+    }
+  })
+  return (
+    <div>
+      <ExpenseFilter selected={filteredyear} onfilter={filterChangeHandler} />;
+      {newItems.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          title={expense.title}
+          amt={expense.amt}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
 export default Expenses;
