1e5e5d0e8a9540093fedff792b1c618eb2d90633
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-09 14:13:25 +0530

    addinmg cancel button

--- src/component/ExpenseForm.js
+++ src/component/ExpenseForm.js
@@ -1,47 +1,65 @@
-import React,{useState} from 'react';
-const ExpenseForm=(props)=>
-{
-    const [enteredTitle,setEnteredTitle]=useState('');
-    const [enteredAmt,setEnteredAmt]=useState('');
-    const [enteredDate,setEnteredDate]=useState('');
-    function Titlehandler(e) 
-    {
-        setEnteredTitle(e.target.value);
-    }
-    function amthandler(e) 
-    {
-        setEnteredAmt(e.target.value);
-    }
-    function datehandler(e) 
-    {
-        setEnteredDate(e.target.value);
-    }
-    const submit=(e)=>{
-     e.preventDefault();
-     const obj={
-        title:enteredTitle,
-        amt:enteredAmt,
-        date:new Date(enteredDate),
-        id:Math.random().toString()
-     }
-     props.onAddEventListner(obj);
-     setEnteredTitle('');
-     setEnteredAmt('');
-     setEnteredDate('');
-     
-    }
-return (
+import React, { useState } from "react";
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmt, setEnteredAmt] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  function Titlehandler(e) {
+    setEnteredTitle(e.target.value);
+  }
+  function amthandler(e) {
+    setEnteredAmt(e.target.value);
+  }
+  function datehandler(e) {
+    setEnteredDate(e.target.value);
+  }
+  const submit = (e) => {
+    e.preventDefault();
+    const obj = {
+      title: enteredTitle,
+      amt: enteredAmt,
+      date: new Date(enteredDate),
+    };
+    props.onAddEventListner(obj);
+    props.setediting();
+    setEnteredTitle("");
+    setEnteredAmt("");
+    setEnteredDate("");
+  };
+  const deleteItem = (e) => {
+    e.preventDefault();
+    props.setediting();
+  };
+  return (
     <div>
-        <form onSubmit={submit}>
-            <label>ExpenseTitle:</label>
-        <input type="text" id="text" value={enteredTitle}autoComplete="off" onChange={Titlehandler}/>
-            <label>ExpenseAmt:</label>
-        <input type="number" id="number" value={enteredAmt} autoComplete="off" onChange={amthandler}/>
-            <label>ExpenseDate:</label>
-        <input type="date" id="date" value={enteredDate}autoComplete="off" onChange={datehandler}/>
-        <button >Submit</button>
-        </form>
+      <form onSubmit={submit}>
+        <label>ExpenseTitle:</label>
+        <input
+          type="text"
+          id="text"
+          value={enteredTitle}
+          autoComplete="off"
+          onChange={Titlehandler}
+        />
+        <label>ExpenseAmt:</label>
+        <input
+          type="number"
+          id="number"
+          value={enteredAmt}
+          autoComplete="off"
+          onChange={amthandler}
+        />
+        <label>ExpenseDate:</label>
+        <input
+          type="date"
+          id="date"
+          value={enteredDate}
+          autoComplete="off"
+          onChange={datehandler}
+        />
+        <button onClick={deleteItem}>Cancel</button>
+        <button>Add Expense</button>
+      </form>
     </div>
-);
-}
+  );
+};
 export default ExpenseForm;
