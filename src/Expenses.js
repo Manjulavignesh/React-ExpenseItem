3364d58149901cf25a3848056ecf6e4be5a5288a
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-09 16:20:58 +0530

    chartapp

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExpenseList from "./ExpenseList";
 import ExpenseFilter from "./component/ExpenseFilter";
+import ExpenseChart from "./ExpenseChart";
 const Expenses = (props) => {
   const [filteredyear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedyear) => {
@@ -17,6 +18,7 @@ const Expenses = (props) => {
     <div>
       <ExpenseFilter selected={filteredyear} onfilter={filterChangeHandler} />;
     <ExpenseList items={newItems}/> 
+    <ExpenseChart items={newItems}/>
     </div>
   );
 };
